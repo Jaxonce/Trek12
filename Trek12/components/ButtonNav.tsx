f5dfe5035da92dc9a83d4f83d3d5ed0742ec056e
Maxence Lanone <maxence.lanone@etu.uca.fr>
2023-02-11 16:56:06 +0100

    ajout bouton components + typo sketch

---
+++ Trek12/components/ButtonNav.tsx
@@ -0,0 +1,54 @@
+import * as React from "react";
+import { View, Text, StyleSheet, Button, Pressable } from "react-native";
+
+//buttonNav props
+type ButtonNavProps = {
+    text: string;
+}
+
+export default function ButtonNav(props: ButtonNavProps){
+  return (
+        <Pressable onPressIn={onPress} style={({ pressed }) => [
+            {
+              opacity: pressed
+                ? 0.8
+                : 1,
+              backgroundColor: '#2277ee'
+            },
+            styles.bigButton,
+          ]}>
+            <Text style={styles.text}>{props.text}</Text>
+          </Pressable>
+    );
+};
+
+function onPress() {
+    console.log("Pressed!");
+}
+
+const styles = StyleSheet.create({
+    bigButton: {
+        width: 265,
+        height: 114,
+        backgroundColor: "rgba(217, 217, 217, 0.53)",
+        borderRadius: 22,
+        boxShadow: '0 12px 6px 0 rgba(0, 0, 0, 0.41)',
+        backdropFilter: 'blur(4px)',
+        borderWidth: 10,
+        borderColor: '#583929',
+        borderStyle: 'solid',
+        flex: 0,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        color: 'black', 
+        fontSize: 30, 
+        fontWeight: 'bold',
+        textAlign: 'center',
+        fontFamily: 'Sketch',
+    }
+});
+
+
---
+++ Trek12/screens/HomeScreen.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, ImageBackground } from 'react-native';
+import ButtonNav from '../components/ButtonNav';
+
+export default function HomeScreen() {
+    return (
+        <ImageBackground source={require('../assets/bg.png')} resizeMode="cover" style={styles.image}>
+            <View style={styles.container}>
+                <ButtonNav text='Creer une partie'></ButtonNav>
+                <ButtonNav text='Rejoindre une partie'></ButtonNav>
+            </View>
+        </ImageBackground>
+        
+    );
+    }
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: '100%',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        flexDirection: 'row',
+    },
+    image: {
+        flex: 1,
+        justifyContent: "center",
+        transform: [{ translateX: 0 }, { translateY: 0 }]
+      },
+});
