580c33a2c80dc1ca5e9616102c9c43adbfd74335
Aurelien.Pintrand <Aurelien.PINTRAND@etu.uca.fr>
2023-02-12 01:00:24 +0100

    restructuration du projet + ajout MCD

---
+++ Source/Trek12/App.tsx
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './screens/HomeScreen';
+import * as ScreenOrientation from 'expo-screen-orientation';
+import { useFonts } from 'expo-font';
+
+
+
+ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE_RIGHT);
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Sketch': require('./assets/fonts/Sketch-Block.ttf'),
+  });
+  if (!fontsLoaded) {
+    return null;
+  }
+  return (
+    <HomeScreen></HomeScreen>
+  );
+}
