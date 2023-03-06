57da4f04ab69f369c8bbb8691dd4cf9dea44cccd
Maxence Lanone <maxence.lanone@etu.uca.fr>
2023-03-06 17:10:22 +0100

    :construction: ajout component calcul operation fin de partie

--- Source/Trek12/components/StartNavigation.tsx
+++ Source/Trek12/components/StartNavigation.tsx
@@ -4,12 +4,13 @@ import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from '../screens/HomeScreen';
 import JoinGameScreen from '../screens/JoinGameScreen';
 import CreateGameScreen from '../screens/CreateGameScreen';
+import EndPartie from '../screens/EndPartie';
 
 export default function StartNavigation() {
     const Stack = createStackNavigator();
     return (
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
+        <Stack.Navigator initialRouteName="EndPartie">
           <Stack.Screen 
             name="Home" 
             component={HomeScreen} 
@@ -23,6 +24,11 @@ export default function StartNavigation() {
           <Stack.Screen
             name="JoinGameScreen"
             component={JoinGameScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="EndPartie"
+            component={EndPartie}
             options={{ headerShown: false }}
           />
         </Stack.Navigator>
---
+++ Source/Trek12/screens/EndPartie.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, ImageBackground, Touchable } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import ButtonNav from '../components/ButtonNav';
+import InputBox from '../components/InputBox';
+import { useNavigation } from '@react-navigation/native';
+import StartNavigation from '../components/StartNavigation';
+import JoinGameScreen from './JoinGameScreen';
+
+
+
+export default function EndScreen({navigation}) {
+    const goToNewPlay = () => {navigation.navigate("CreateGameScreen")}
+    const goToOther = () => {navigation.navigate("JoinGameScreen")}
+    return (
+        <ImageBackground source={require('../assets/bg.png')} resizeMode="cover" style={styles.image}>
+        </ImageBackground>
+        
+    );
+    }
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: '100%',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        flexDirection: 'row',
+    },
+    image: {
+        flex: 1,
+        justifyContent: "center",
+        transform: [{ translateX: 0 }, { translateY: 0 }]
+      },
+});
