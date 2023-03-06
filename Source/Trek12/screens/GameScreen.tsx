c4b5d27b301a2467d42091bbc889d63ce5c8aa5a
Theo DUPIN <Theo.DUPIN@etu.uca.fr>
2023-03-06 17:28:06 +0100

    Image de fond jeu intégrée + le nombre de tours

--- Source/Trek12/screens/GameScreen.tsx
+++ Source/Trek12/screens/GameScreen.tsx
@@ -1,26 +1,25 @@
 import React, {useRef, useEffect} from 'react';
-import { Text, View, StyleSheet, SafeAreaView, Image, Dimensions } from 'react-native';
+import { Text, View, StyleSheet, SafeAreaView, Image, Dimensions, ImageBackground } from 'react-native';
 
 
 export default function GameScreen() {
-    const { width, height } = Dimensions.get('window');
     return (
-        <View style={styles.backgroundImage}>
-            <Image source={require(".././assets/bg_game.png")} style={{width, height}}/>
-        </View>
+        <ImageBackground source={require(".././assets/bg_game.png")} style={styles.backgroundImage}>
+            <Text style={{fontFamily: "Sketch", fontSize: 30,}}>1/19</Text>
+        </ImageBackground>
     );
 }
 
 const styles = StyleSheet.create({
     backgroundImage: {
     flex: 1,
-    position: 'absolute',
     top: 0,
     left: 0,
     right: 0,
     bottom: 0,
-    // L'image de fond
-    source: require('.././assets/bg_game.png'),
+    jusrifyContent: 'center',
+    alignItems: 'center',
+    transform:[{translateX: 0}, {translateY: 0}]
     },
 });
 
