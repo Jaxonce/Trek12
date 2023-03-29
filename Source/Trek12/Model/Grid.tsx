e3576237db7087d3e9011cc004839a1d7f1afafc
Aurelien PINTRAND <Aurelien.PINTRAND@etu.uca.fr>
2023-03-29 23:55:02 +0200

    :hammer::beers: update méthode countZones, un peu gitan mais si mes calculs sont bons ( et il le sont toujours ) normalement le parcours de graphe devrait marcher . Mais reste à régler le pb des zones .

--- Source/Trek12/Model/Grid.tsx
+++ Source/Trek12/Model/Grid.tsx
@@ -30,6 +30,11 @@ class Grid {
     return undefined;
   }
 
+  public getAdjacentCells(id: number): number[] {
+    const adjacentIds = this.getTemplate().getGraph().get(id) ?? [];
+    return adjacentIds;
+  }
+
   public getCellValue(id: number): number {
     return this.findCellById(id).getValue();
   }
@@ -63,32 +68,72 @@ class Grid {
     return Math.max(...valueChaineTrie) + (valueChaineTrie.length - 1);
   }
 
-  public CalculAndFindScoreZone(listeId: Array<number>): number {
-    const cells = [];
-    const template = this.Template.getGraph();
-    const valueZone = [];
-    for (const cell of listeId) {
-      valueZone.push(this.getCellValue(cell));
-      cells.push(this.findCellById(cell));
+  public CalculAndFindScoreZone(l: Array<number>): number {
+    var listeVisited = [] // liste des cellules visitée
+    var listeCellulesZone = [] // liste des cellules de la zone actuelle
+    var zoneCells: Cell[] = [] //  cellules de la zone
+
+    // si la liste donnée en param contient pas des id identiques, ça dégage parce que c'est pas une zone
+    if (l.every((id, index, array) => id == array[0])) {
+      return -1
     }
-    for (let i = 1; i < valueZone.length; i++) {
-      if (!(valueZone[i] == valueZone[i - 1]) || !(listeId[i] in template.get(listeId[i - 1]))) {
-        return -1;
+
+    //pour toutes les cellules demandées
+    for (const cellId of l) {
+
+      if (!listeVisited.includes(cellId)) {       // si la cellule n'est pas déjà visitée
+
+        listeVisited.push(cellId) // la marque comme visitée
+        for (const adj of this.getAdjacentCells(cellId)) { // pour chacune de ses cellules adjacentes
+
+          if ((!listeCellulesZone.includes(adj)) && (l.includes(adj))) { // si elle est pas dans listeZone mais qu'elle est dans la liste donnée en param 
+
+            listeCellulesZone.push(adj) // l'ajoute à la liste des cellules de la zone
+
+            if (!listeCellulesZone.includes(cellId)) { // si la cellule de base ( pas l'adjacente ) est pas déjà dans la zone
+              listeCellulesZone.push(cellId) // ajoute la cellule de base dans la zone
+            }
+          }
+
+          else if (!l.includes(adj)) { // sinon si l'adjacent est pas dans la liste des param
+            if (!listeVisited.includes(adj)) { // si elle est pas marquée visitée
+              listeVisited.push(adj) // la marque comme visitée
+            }
+          }
+        }
+
       }
     }
-    for (const cell of cells) {
-      if (cell.getType() == Celltype.ZONE || cell.getType() == Celltype.BOTH) {
-        return -1;
+
+    // si les cellules de la zone sont toutes dans la liste en param et que la longueur des deux listes est la même
+    // ( permet de vérifier que tout se soit bien passé comme il faut )
+    if ((listeCellulesZone.every((element) => l.includes(element))) && l.length == listeCellulesZone.length) {
+
+      //ajoute dans la liste des cellules de la zone ( pas la liste de leurs ids) les cellules
+      for(const idCell of listeCellulesZone){
+        zoneCells.push(this.findCellById(idCell))
       }
-    }
-    for (const cell of cells) {
-      if (cell.getType() == Celltype.CHAIN) {
-        cell.setType(Celltype.BOTH);
-      } else if (cell.getType() == Celltype.UNKNOWN) {
-        cell.setType(Celltype.ZONE);
+
+      /*
+      for(const cell of zoneCells){ //pour toutes les cellules de la zone
+
+        // si c'est déjà une zone return -1
+        if((cell.getType()== Celltype.ZONE) || (cell.getType()==Celltype.BOTH)){
+          return -1
+        }
+
+        if(cell.getType() == Celltype.CHAIN){ // si c'est déjà une chaîne set à BOTH
+          cell.setType(Celltype.BOTH)
+        }
+
+        else{ // sinon set à ZONE
+          cell.set
+        }
       }
+      */
+     //méthode pas finie pour le moment, faut régler le pb de l'appartenance aux zones
+     return 69
     }
-    return valueZone[0] + (valueZone.length - 1);
   }
 
   public getCells(): Cell[] {
