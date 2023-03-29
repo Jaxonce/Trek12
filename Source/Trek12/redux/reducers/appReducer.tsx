b5847a8bbb82ebabce83b43c5a341276c0511804
Theo DUPIN <Theo.DUPIN@etu.uca.fr>
2023-03-29 10:31:00 +0200

    Création du dossier API avec un Thunk

--- Source/Trek12/redux/reducers/appReducer.tsx
+++ Source/Trek12/redux/reducers/appReducer.tsx
@@ -1,14 +1,20 @@
-import { ID_PLAYER, PSEUDO_PLAYER, CURRENT_GAME, CURRENT_PLAYER } from "../constants";
+import { CREATE_GAME, CREATE_GAMEFACTORY, CURRENT_GAME, CURRENT_PLAYER } from "../constants";
+import Player from "../../Model/Player";
+import Game from "../../Model/Game";
+import OperationManager from "../../Model/OperationManager";
 
 
 const initialState = {
-    player: [],
-    current_game: []
-  }
+  player: new Player
+}
   
   // @ts-ignore
   export function appReducer (state = initialState, action) {
     switch (action.type) {
+      case CREATE_GAMEFACTORY:
+        return {...state, gameFactory: action.payload};
+      case CREATE_GAME:
+        return {...state, game: action.payload};
       case CURRENT_PLAYER:
         return {...state, player: action.payload};
       case CURRENT_GAME:
