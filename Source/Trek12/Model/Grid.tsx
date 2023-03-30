f976818978a1c6bff6c7c40b66db918ac92ccf81
Aurelien PINTRAND <Aurelien.PINTRAND@etu.uca.fr>
2023-03-30 08:50:44 +0200

    :sparkles: finished zone algorithm ( i think )

--- Source/Trek12/Model/Grid.tsx
+++ Source/Trek12/Model/Grid.tsx
@@ -72,6 +72,7 @@ class Grid {
     var listeVisited = [] // liste des cellules visitée
     var listeCellulesZone = [] // liste des cellules de la zone actuelle
     var zoneCells: Cell[] = [] //  cellules de la zone
+    var celltypes : Celltype[] = []
 
     // si la liste donnée en param contient pas des id identiques, ça dégage parce que c'est pas une zone
     if (l.every((id, index, array) => id == array[0])) {
@@ -114,25 +115,32 @@ class Grid {
         zoneCells.push(this.findCellById(idCell))
       }
 
-      /*
-      for(const cell of zoneCells){ //pour toutes les cellules de la zone
+      // récupère le type de chaque cellule de la zone
+      for (const cell of zoneCells){
+        celltypes.push(cell.getType())
+      }
 
-        // si c'est déjà une zone return -1
-        if((cell.getType()== Celltype.ZONE) || (cell.getType()==Celltype.BOTH)){
-          return -1
-        }
+      // si y'en a à zone ou both return -1
+      if(celltypes.includes(Celltype.ZONE) || celltypes.includes(Celltype.BOTH)){
+        return -1
+      }
+
+      for(const cell of zoneCells){ //pour toutes les cellules de la zone
 
         if(cell.getType() == Celltype.CHAIN){ // si c'est déjà une chaîne set à BOTH
           cell.setType(Celltype.BOTH)
         }
 
         else{ // sinon set à ZONE
-          cell.set
+          cell.setType(Celltype.ZONE)
         }
       }
-      */
-     //méthode pas finie pour le moment, faut régler le pb de l'appartenance aux zones
-     return 69
+
+     //return le score
+      return zoneCells[0].getValue() + zoneCells.length-1
+    }
+    else {
+      return -1
     }
   }
 
